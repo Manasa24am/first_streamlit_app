2d676ffd9d95cf4fc5eeaacf3b578de0edd93544
Manasa24am <106089796+Manasa24am@users.noreply.github.com>
2022-05-24 10:29:21 +0530

    Update streamlit_app.py
    Added menu list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy dinner');
+
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 & blueberry oatmeal');
+streamlit.text('Kale, spinach & rocket smothie');
+streamlit.text('Hard-boiled Free-range egg');
